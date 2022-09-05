3f3fbf44868c255f0511bab1acc5e7455d540f39
Rajeta <rajeta@gmail.com>
2022-09-04 19:04:23 -0500

    Initial structure of script

---
+++ script.js
@@ -0,0 +1,7 @@
+//function getComputerChoice will randomly return rock, paper, or scissors and save result to a variable.
+
+//prompt player to enter choice of rock, paper, or scissors and save result to a variable
+
+//create function that collects computer's choice and player's choice, compares the two, and declares the winner.
+
+//function game will play 5 round game and declare winner and loser at the end.
