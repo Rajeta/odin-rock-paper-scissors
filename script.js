b3519d2d7406e10c5fafed2cc5f7645175c0e2b5
Rajeta <rajeta@gmail.com>
2022-09-04 19:17:34 -0500

    create function that gets computer's choice

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 //function getComputerChoice will randomly return rock, paper, or scissors and save result to a variable.
+function getComputerChoice()    {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let computerSelection;
+    if (randomNumber === 1) {
+        computerSelection = "Rock";
+    } else if (randomNumber === 2) {
+        computerSelection = "Paper";
+    } else if (randomNumber === 3) {
+        computerSelection = "Scissors";
+    }
+    return(computerSelection);
+}
 
 //prompt player to enter choice of rock, paper, or scissors and save result to a variable
 
